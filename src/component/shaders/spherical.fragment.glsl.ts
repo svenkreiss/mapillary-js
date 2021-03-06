d01b492e6b331ba5183d9ce1de87fc3b80dfa2f5
Oscar Lorentzon <oscarlorentzon@users.noreply.github.com>
2021-03-06 18:28:32 +0100

    feat: image depth blending in shader

--- src/component/shaders/spherical.fragment.glsl.ts
+++ src/component/shaders/spherical.fragment.glsl.ts
@@ -11,15 +11,16 @@ uniform sampler2D projectorTex;
 uniform float opacity;
 
 varying vec4 vRstq;
+varying vec4 depthColor;
 
-void main()
-{
+void main() {
     vec3 b = normalize(vRstq.xyz);
     float lat = -asin(b.y);
     float lng = atan(b.x, b.z);
     float x = lng / tau + 0.5;
     float y = lat / tau * 2.0 + 0.5;
     vec4 baseColor = texture2D(projectorTex, vec2(x, y));
+    baseColor = mix(baseColor, depthColor, 0.75);
     baseColor.a = opacity;
     gl_FragColor = baseColor;
 }
